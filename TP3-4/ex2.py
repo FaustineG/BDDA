b760aeecd7060ac266bbcc73c451de5e10e51bac
Faustine Godbillot <faustiine35@gmail.com>
2018-03-19 17:57:41 +0100

    Avancee sur l'ex2 tp3-4

--- TP3-4/ex2.py
+++ TP3-4/ex2.py
@@ -2,5 +2,18 @@ import findspark
 findspark.init('/opt/spark')
 
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
 
-DATA_FILE="onlyBayLocsUsersWithFreq.txt"
+
+# book1.json a book1560.json
+DATA_FILE="books-json/*.json"
+
+def main():
+	conf = SparkConf()
+	sc = SparkContext(appName="ex2.py", conf=conf)
+	spark = SparkSession.builder.appName("ex2.py").getOrCreate()
+	df = spark.read.json(DATA_FILE, multiLine = True)
+	print(df.printSchema())
+
+if __name__ == "__main__":
+	main()
